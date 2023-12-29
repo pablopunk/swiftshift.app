e142ffa13ffc5eafc6d944915a0b90eaab2d769a
Pablo P Varela <pablo@pablopunk.com>
2023-12-29 15:16:38 +0100

    fix header

--- src/site.ts
+++ src/site.ts
@@ -10,7 +10,7 @@ const site = {
   SITE_NAME: "Swift Shift | Manage your mac's windows like a pro",
   SITE_DESC:
     "Swift Shift is a macOS app that lets you assign shortcuts to move & resize windows. It's the easiest and fastest way to organize your workspace",
-  SITE_IMAGE: `${siteUrl}/images/header-light.png`,
+  SITE_IMAGE: `${siteUrl}/header-light.png`,
   SITE_COPYRIGHT: "Pablo Varela",
   SITE_REPO: "https://github.com/pablopunk/swiftshift.app",
 };
