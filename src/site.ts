9ff25c711214e8631388984a1422a1d1d32a7e32
Pablo P Varela <pablo@pablopunk.com>
2024-06-07 11:39:08 +0200

    quadrants

--- src/site.ts
+++ src/site.ts
@@ -1,55 +1,45 @@
 export const mainUrl = "https://swiftshift.app"
 
-type NodeEnv = "development" | "production"
-interface ProcessEnv {
-	NODE_ENV: NodeEnv
-	NEXT_PUBLIC_VERCEL_URL?: string
-}
-interface Process {
-	env: ProcessEnv
-}
-
-const processEnv = (process as any as Process).env
+const processEnv = import.meta.env
 const vercelUrl = processEnv.NEXT_PUBLIC_VERCEL_URL
-const url =
-	processEnv.NODE_ENV === "development"
-		? "http://localhost:4321"
-		: `https://${vercelUrl || mainUrl}`
+const url = processEnv.DEV
+  ? "http://localhost:4321"
+  : `https://${vercelUrl || mainUrl.replace("https://", "")}`
 
 const site = {
-	SITE_URL: url,
-	SITE_NAME: "Swift Shift | Manage your mac's windows like a pro",
-	SITE_DESC:
-		"Swift Shift is a macOS app that lets you assign shortcuts to move & resize windows. It's the easiest and fastest way to organize your workspace.",
-	SITE_IMAGE: `${url}/header-light-extended.png`,
-	SITE_COPYRIGHT: "Pablo Varela",
-	SITE_REPO: "https://github.com/pablopunk/swiftshift.app",
-	SITE_KEYWORDS: [
-		"mac",
-		"os",
-		"app",
-		"macosapp",
-		"window",
-		"manager",
-		"swift",
-		"shift",
-		"shortcut",
-		"move",
-		"resize",
-		"keyboard",
-		"productivity",
-		"workspace",
-		"organize",
-		"efficient",
-		"fast",
-		"easy",
-		"free",
-		"open",
-		"source",
-		"pablopunk",
-		"pablovarela",
-		"swiftshift",
-	],
+  SITE_URL: url,
+  SITE_NAME: "Swift Shift | Manage your mac's windows like a pro",
+  SITE_DESC:
+    "Swift Shift is a macOS app that lets you assign shortcuts to move & resize windows. It's the easiest and fastest way to organize your workspace.",
+  SITE_IMAGE: `${url}/header-light-extended.png`,
+  SITE_COPYRIGHT: "Pablo Varela",
+  SITE_REPO: "https://github.com/pablopunk/swiftshift.app",
+  SITE_KEYWORDS: [
+    "mac",
+    "os",
+    "app",
+    "macosapp",
+    "window",
+    "manager",
+    "swift",
+    "shift",
+    "shortcut",
+    "move",
+    "resize",
+    "keyboard",
+    "productivity",
+    "workspace",
+    "organize",
+    "efficient",
+    "fast",
+    "easy",
+    "free",
+    "open",
+    "source",
+    "pablopunk",
+    "pablovarela",
+    "swiftshift",
+  ],
 }
 
 export default site
