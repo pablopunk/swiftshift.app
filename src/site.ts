cc3d934617a20c853aa2a0a0139f5fa098daa579
Pablo P Varela <pablo@pablopunk.com>
2024-06-28 20:17:35 +0200

    fix description and details positioning in mobile

--- src/site.ts
+++ src/site.ts
@@ -10,7 +10,7 @@ const site = {
   SITE_URL: url,
   SITE_NAME: "Swift Shift | Manage your mac's windows like a pro",
   SITE_DESC:
-    "Swift Shift is a macOS app that lets you assign shortcuts to move & resize windows. It's the easiest and fastest way to organize your workspace.",
+    "Swift Shift lets you move/resize windows with your mouse without searching for tiny arrows or window titles. It's the fastest way to organize your workspace to your liking.",
   SITE_IMAGE: `${url}/header-light-extended.png`,
   SITE_COPYRIGHT: "Pablo Varela",
   SITE_REPO: "https://github.com/pablopunk/swiftshift.app",
